f8a5c78fc1a89eb5f92e855541b91f1d06467a61
IevgeniiaAbdulina <ievgeniiaabdulina@gmail.com>
2019-12-27 22:15:44 +0100

    Header
    // Corrected a header application: added Header.css link to the Header.js file.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,13 @@
+import "./Header.css";
 import React from "react";
 
 class Header extends React.Component {
   render() {
     return (
       <div className="ui grid">
-        <div
-          className="content four wide column"
-          style={{
-            maxWidth: "170px",
-            marginTop: "20px",
-            fontFamily: "'Roboto', sans - serif",
-            letterSpacing: "- 1.5%",
-            fontWeight: "bold",
-            lineHeight: "0.6",
-            textTransform: "uppercase",
-            textAlign: "right"
-          }}
-        >
-          <div
-            className="ui sub orange header"
-            style={{
-              fontSize: "2.5em"
-            }}
-          >
-            Cinema
-          </div>
-          App
+        <div className="content">
+          <div className="ui sub header">Cinema</div>
+          <div className="app-header">App</div>
         </div>
       </div>
     );
