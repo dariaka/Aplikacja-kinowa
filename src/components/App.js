25552df97c2546ac230eb5d949d538b565569d5c
Daria <daria359@gmail.com>
2020-01-02 18:58:30 +0100

    Add state to App component

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,49 @@ import React from 'react';
 import Header from './Header';
 import Repertoire from './Repertoire';
 import OrderPanel from './OrderPanel';
+import Modal from './Modal';
 
 class App extends React.Component {
-    state = {};
-  
+    state = {
+        panel: 'dev-mode',
+        showModal: false,
+        selectedDay: null,
+        movies: [],
+        // movies: [{movie: {fetch data from movie database}, sessions: [fetch data from session database]}, {}, ..., {}] ??
+        selectedMovie: null,
+        selectedSession: null,
+    };
+
     componentDidMount() {}
-  
+
     render() {
-      return (
-        <div className="ui container">
-          <Header />
-          <Repertoire />
-          <OrderPanel />
-        </div>
-      );
+        if (this.state.panel === 'repertoire') {
+            return (
+                <div className="ui container">
+                    <Header />
+                    <Repertoire />
+                </div>
+            );
+        }
+        if (this.state.panel === 'order') {
+            return (
+                <div className="ui container">
+                    <Header />
+                    <OrderPanel />
+                    <Modal />
+                </div>
+            );
+        }
+        if (this.state.panel === 'dev-mode') {
+            return (
+                <div className="ui container">
+                    <Header />
+                    <Repertoire />
+                    <OrderPanel />
+                </div>
+            );
+        }
     }
-  }
-  
-  export default App;
-  
+}
+
+export default App;
