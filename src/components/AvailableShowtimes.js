40a1314ac478f0b9901aee671ff0eeb2e1f1b0d6
IevgeniiaAbdulina <ievgeniiaabdulina@gmail.com>
2020-01-05 20:10:29 +0100

    Application ui
    // Added CSS style to the elements

--- src/components/AvailableShowtimes.js
+++ src/components/AvailableShowtimes.js
@@ -1,35 +1,36 @@
-import './AvailableShowtimes.css';
-import React from 'react';
-import Button from './Button';
+import "./AvailableShowtimes.css";
+import React from "react";
+import Button from "./Button";
 
 class AvailableShowtimes extends React.Component {
+  ifActive = time => {
+    return !time.fromNow().includes("ago");
+  };
 
-    ifActive = (time) => {
-        return !time.fromNow().includes('ago');
-    }
+  goToReservationPanel = (movie, session) => {
+    this.props.onSessionClick(movie, session);
+  };
 
-    goToReservationPanel = (movie, session) => {
-        this.props.onSessionClick(movie, session);
-    }
+  renderedList = this.props.sessions.map(session => {
+    return (
+      <Button
+        key={session.id}
+        text={session.time.format("HH:mm")}
+        active={this.ifActive(session.time)}
+        onButtonClick={() =>
+          this.goToReservationPanel(this.props.movie, session)
+        }
+      />
+    );
+  });
 
-    renderedList = this.props.sessions.map(session => {
-        return (
-            <Button 
-                key={session.id}
-                text={session.time.format('HH:mm')} 
-                active={this.ifActive(session.time)}
-                onButtonClick={() => this.goToReservationPanel(this.props.movie, session)} 
-            />
-        );
-    })
-
-    render() {
-        return (
-            <div className="wrapper">
-                {this.renderedList}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="ui grid" style={{ margin: "40px" }}>
+        {this.renderedList}
+      </div>
+    );
+  }
 }
 
 export default AvailableShowtimes;
--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -1,14 +1,36 @@
-import './MovieInfo.css';
-import React from 'react';
+import "./MovieInfo.css";
+import React from "react";
 
-function MovieInfo({ title, summary, image }) {
+function MovieInfo({ title, summary, image, isVertical }) {
+  if (isVertical) {
     return (
-        <div className="wrapper">
+      <div className="ui grid">
+        <div className="two column row">
+          <div className="six wide column">
+            <img className="ui small image" src={image} alt={title} />
+          </div>
+        </div>
+        <div className="twelve wide column" style={{ margin: "20px 0" }}>
+          <h4>{title}</h4>
+          <p>{summary}</p>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="ui grid">
+        <div className="two column row" style={{ margin: "20px 0" }}>
+          <div className="six wide column">
+            <img className="ui small image" src={image} alt={title} />
+          </div>
+          <div className="ten wide column" style={{ margin: "20px 0" }}>
             <h4>{title}</h4>
             <p>{summary}</p>
-            <img className="ui small image" src={image} alt={title} />
+          </div>
         </div>
+      </div>
     );
+  }
 }
 
 export default MovieInfo;
