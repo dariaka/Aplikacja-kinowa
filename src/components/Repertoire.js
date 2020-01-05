cdd3134d58ff8c8220d91eb4afcdd3903351f961
Daria <daria359@gmail.com>
2020-01-05 01:52:52 +0100

    Fix issue with a warning in a web browser's console

--- src/components/Repertoire.js
+++ src/components/Repertoire.js
@@ -54,13 +54,13 @@ class Repertoire extends React.Component {
 
     renderedList = this.days.map((day, id) => {
         return (
-            <a key={id}
+            <div key={id}
                 onClick={this.onDayClicked}
                 id={"day" + id}
                 className={id === this.state.activeDayId ? "active item" : "item"}
-                style={{ paddingLeft: "20px" }} >
+                style={{ cursor: "pointer", paddingLeft: "20px" }} >
                 {id === 0 ? day.format("[Today]") : day.format("dddd")}
-            </a>
+            </div>
         );
     })
     
