daea22243ec72a437ac502b7f6925554e065114e
IevgeniiaAbdulina <ievgeniiaabdulina@gmail.com>
2020-01-05 14:48:39 +0100

    Seats correct

--- src/components/Place.js
+++ src/components/Place.js
@@ -5,6 +5,11 @@ function Place(props) {
   // Reprezentuje jedno siedzenie w kinie - może mieć stan wolne, zajęte i wybrane - początkowy stan oznaczany jest przy renderowaniu na podstawie bazy danych.
   // Kliknięcie w zajęte miejsce nie powoduje żadnej akcji.
   // Kliknięcie w wolne miejsce zmienia flagę wybrane na true i dodaje miejsce do listy zajętych miejsc którą tworzymy lokalnie - dopiero kiedy użytkownik zatwierdzi operację odpowiednim przyciskiem dodajemy zarezerwowane miejsca do listy poprzednio zarezerwowanych w bazie danych.
+  return (
+    <button className="small ui green button" onClick={props.onButtonClick}>
+      {props.num}
+    </button>
+  );
 }
 
 export default Place;
