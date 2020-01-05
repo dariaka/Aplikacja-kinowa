a327e2b9f936ad6b652055e716b6bbdcd0516370
Daria <daria359@gmail.com>
2020-01-05 21:44:29 +0100

    Add unselecting seats in <Place> component

--- src/components/OrderPanel.js
+++ src/components/OrderPanel.js
@@ -1,73 +1,78 @@
-import "./OrderPanel.css";
-import React from "react";
-import InfoPanel from "./InfoPanel";
-import PlacesSelectionPanel from "./PlacesSelectionPanel";
+import './OrderPanel.css';
+import React from 'react';
+import InfoPanel from './InfoPanel';
+import PlacesSelectionPanel from './PlacesSelectionPanel';
 
 class OrderPanel extends React.Component {
-  state = {
-    reservedSeats: []
-  };
+    state = {
+        reservedSeats: [],
+    };
 
-  onPlaceSelect = seat => {
-      const newList = this.state.reservedSeats.concat(seat);
-      this.setState({
-          reservedSeats: newList
-      });
-  }
+    onPlaceSelect = seat => {
+        let newList = this.state.reservedSeats;
+        if (this.state.reservedSeats.some(reservedSeat => reservedSeat.row === seat.row && reservedSeat.place === seat.place)) {
+            newList = this.state.reservedSeats.filter(reservedSeat => !(reservedSeat.row === seat.row && reservedSeat.place === seat.place));
+        } else {
+            newList = this.state.reservedSeats.concat(seat);
+        }
+        this.setState({
+            reservedSeats: newList,
+        });
+    };
 
-  render() {
-    return (
-      <div className="ui container">
-        <div className="ui text menu" style={{ padding: "40px 0" }}>
-          <div className="header item" style={{ fontSize: "1.25em" }}>
-            Booking
-          </div>
-        </div>
-        <div className="ui grid">
-          <div className="six wide column">
-            <div className="five wide column">
-              <InfoPanel
-                movie={this.props.movie}
-                session={this.props.session}
-                reservedSeats={this.state.reservedSeats}
-                onOrderSubmit={this.props.onOrderSubmit}
-              />
-            </div>
-          </div>
-
-          <div className="ten wide column">
-            <div className="column">
-              <h3 className="red">Select seats</h3>
-              <div className="ui divider"></div>
-            </div>
-
-            <div className="ui four column grid">
-              <div className="row">
-                <div className="column">
-                  <i className="square full green icon"></i>
-                  Available
+    render() {
+        return (
+            <div className="ui container">
+                <div className="ui text menu" style={{ padding: '40px 0' }}>
+                    <div className="header item" style={{ fontSize: '1.25em' }}>
+                        Booking
+                    </div>
                 </div>
-                <div className="column">
-                  <i className="square full grey icon"></i>
-                  Unavailable
+                <div className="ui grid">
+                    <div className="six wide column">
+                        <div className="five wide column">
+                            <InfoPanel
+                                movie={this.props.movie}
+                                session={this.props.session}
+                                reservedSeats={this.state.reservedSeats}
+                                onOrderSubmit={this.props.onOrderSubmit}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="ten wide column">
+                        <div className="column">
+                            <h3 className="red">Select seats</h3>
+                            <div className="ui divider"></div>
+                        </div>
+
+                        <div className="ui four column grid">
+                            <div className="row">
+                                <div className="column">
+                                    <i className="square full green icon"></i>
+                                    Available
+                                </div>
+                                <div className="column">
+                                    <i className="square full grey icon"></i>
+                                    Unavailable
+                                </div>
+                                <div className="column">
+                                    <i className="square full orange icon"></i>
+                                    Your choice
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <PlacesSelectionPanel
+                                session={this.props.session}
+                                onPlaceSelect={this.onPlaceSelect}
+                            />
+                        </div>
+                    </div>
                 </div>
-                <div className="column">
-                  <i className="square full orange icon"></i>
-                  Your choice
-                </div>
-              </div>
             </div>
-            <div>
-              <PlacesSelectionPanel
-                session={this.props.session}
-                onPlaceSelect={this.onPlaceSelect}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default OrderPanel;
