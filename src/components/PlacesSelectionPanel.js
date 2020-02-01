757cf9702e78325e2b8723e839fe2401a83b15ac
Daria <daria359@gmail.com>
2020-02-01 15:02:49 +0100

    Refactoring: convert PlacesSelectionPanel to functional component

--- src/components/PlacesSelectionPanel.js
+++ src/components/PlacesSelectionPanel.js
@@ -2,47 +2,45 @@ import React from 'react';
 import PlacesGrid from './PlacesGrid';
 import './PlacesSelectionPanel.css';
 
-class PlacesSelectionPanel extends React.Component {
-    render() {
-        return (
-            <div className="ui container">
-                <div className="column">
-                        <h3 className="red">Select seats</h3>
-                        <div className="ui divider"></div>
-                    </div>
+function PlacesSelectionPanel (props) {
+    return (
+        <div className="ui container">
+            <div className="column">
+                    <h3 className="red">Select seats</h3>
+                    <div className="ui divider"></div>
+                </div>
 
-                    <div className="ui four column grid">
-                        <div className="row">
-                            <div className="column">
-                                <i className="square full green icon"></i>
-                                Available
-                            </div>
-                            <div className="column">
-                                <i className="square full grey icon"></i>
-                                Unavailable
-                            </div>
-                            <div className="column">
-                                <i className="square full orange icon"></i>
-                                Your choice
-                            </div>
-                        </div>
-                    </div>
-                <div className="ui centered grid">
+                <div className="ui four column grid">
                     <div className="row">
-                        <div className="center aligned ten wide column">
-                            Screen
+                        <div className="column">
+                            <i className="square full green icon"></i>
+                            Available
+                        </div>
+                        <div className="column">
+                            <i className="square full grey icon"></i>
+                            Unavailable
+                        </div>
+                        <div className="column">
+                            <i className="square full orange icon"></i>
+                            Your choice
                         </div>
                     </div>
                 </div>
-                <div>
-                    <PlacesGrid
-                        session={this.props.session}
-                        onPlaceSelect={this.props.onPlaceSelect}
-                    />
+            <div className="ui centered grid">
+                <div className="row">
+                    <div className="center aligned ten wide column">
+                        Screen
+                    </div>
                 </div>
             </div>
-        );
-    }
+            <div>
+                <PlacesGrid
+                    session={props.session}
+                    onPlaceSelect={props.onPlaceSelect}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default PlacesSelectionPanel;
