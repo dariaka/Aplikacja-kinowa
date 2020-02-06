0b655dad98be93e6c3e8362b5f744d8c81c5300f
Daria <daria359@gmail.com>
2020-02-06 03:44:47 +0100

    Updates

---
+++ precache-manifest.298443f8a9157b4804c7dd50954e1faf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29edc3b34efae23785c786db775082e9",
+    "url": "/Aplikacja-kinowa/index.html"
+  },
+  {
+    "revision": "e66da8aea01200aed6ce",
+    "url": "/Aplikacja-kinowa/static/css/main.3936dc81.chunk.css"
+  },
+  {
+    "revision": "3f58155de9792686d265",
+    "url": "/Aplikacja-kinowa/static/js/2.06ed3ee6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Aplikacja-kinowa/static/js/2.06ed3ee6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e66da8aea01200aed6ce",
+    "url": "/Aplikacja-kinowa/static/js/main.a9ee49b6.chunk.js"
+  },
+  {
+    "revision": "598539903fdaf3dcdc8c",
+    "url": "/Aplikacja-kinowa/static/js/runtime-main.5ff9c7e5.js"
+  }
+]);
